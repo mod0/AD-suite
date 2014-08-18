ccb6e34d8cb0bc20a6fbe3d188b35ef90b60f719
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2014-08-18 21:05:20 +0000

    Working version of the split mode fixed point iteration template usign the revolve algorithm.

--- revsplit/iaddr.c
+++
@@ -1,8 +0,0 @@
-/* ##################################################### */ 
-/* This file is part of OpenAD released under the LGPL.  */ 
-/* The full COPYRIGHT notice can be found in the top     */ 
-/* level directory of the OpenAD distribution            */ 
-/* ##################################################### */
-long iaddr_(void* my_active) {
-  return (long) my_active;
-}
