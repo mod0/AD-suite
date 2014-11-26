7699e31e0ec1fe25dba62389f8348b1a754b8c5d
Sri Hari Krishna Narayanan <sriharikrishna@gmail.com>
2014-11-26 12:28:19 +0000

    Copied the split mode template

--- revolve/oad_template_fixedpointstage_withconvergence.f90
+++ revolve/oad_template_fixedpointstage_withconvergence.f90
@@ -60,22 +60,26 @@ integer myi
       our_rev_mode=our_orig_mode
     end if
     if (our_rev_mode%tape) then
+      !If isinloop==0, then call phi(..) in plain mode once
+      !If isinloop==1, then call phi(..) in plain mode till convergence, then call phi(..) once in tape mode to inject partials onto the stack correctly
+      !If isinloop==2, then call phi(..) in tape mode once 
       our_orig_mode=our_rev_mode
       if(isinloop.eq.0) then
         CONV_FLAG = .false.
         ADJ_CONV_FLAG = .false.
         iter = 0
-        adj_iter = 0       
+        adj_iter = 0      
+        !print *, "FW:0  ", double_tape_pointer, integer_tape_pointer, our_rev_mode%plain, our_rev_mode%tape, our_rev_mode%adjoint
       end if
       if(isinloop.eq.1) then
+        IF (ITER .EQ. 0) then
+            !print *, "FW:1-1", double_tape_pointer, integer_tape_pointer, our_rev_mode%plain, our_rev_mode%tape, our_rev_mode%adjoint
+        ENDIF
         IF (.not. (CONV_FLAG)) THEN
           ITER = (ITER+1)
-          !Store the stack pointers
-          temp_double_tape_pointer = double_tape_pointer   
-          temp_integer_tape_pointer = integer_tape_pointer     
-          temp_logical_tape_pointer = logical_tape_pointer    
-          temp_character_tape_pointer = character_tape_pointer 
-          temp_stringlength_tape_pointer = stringlength_tape_pointer
+          our_rev_mode%plain=.TRUE.
+          our_rev_mode%tape=.FALSE.
+          our_rev_mode%adjoint=.FALSE.
           CALL phi(U,U_IP1,B,H,BETA_FRIC)
           normDiff = 0.0
           normZ = 0.0
@@ -91,37 +95,32 @@ integer myi
           if (normDIff/(normZ + 1.0).le.tol) then
             conv_flag = .TRUE.
           endif 
-          if (conv_flag.neqv..true. .AND. iter.ne.n_nl) then
-            !Restore the stack pointers
-            double_tape_pointer = temp_double_tape_pointer   
-            integer_tape_pointer = temp_integer_tape_pointer     
-            logical_tape_pointer = temp_logical_tape_pointer    
-            character_tape_pointer = temp_character_tape_pointer 
-            stringlength_tape_pointer = temp_stringlength_tape_pointer
+          if (conv_flag.eqv..true. .OR. iter.eq.n_nl) then
+            our_rev_mode%plain=.FALSE.
+            our_rev_mode%tape=.TRUE.
+            our_rev_mode%adjoint=.FALSE.
+            CALL phi(U,U_IP1,B,H,BETA_FRIC)
+            !print *, "FW:1-2", double_tape_pointer, integer_tape_pointer, our_rev_mode%plain, our_rev_mode%tape, our_rev_mode%adjoint
           endif 
         ENDIF
       endif
       if(isinloop.eq.0) then
-        !Store the stack pointers
-        temp_double_tape_pointer = double_tape_pointer   
-        temp_integer_tape_pointer = integer_tape_pointer     
-        temp_logical_tape_pointer = logical_tape_pointer    
-        temp_character_tape_pointer = character_tape_pointer 
-        temp_stringlength_tape_pointer = stringlength_tape_pointer
+        our_rev_mode%plain=.TRUE.
+        our_rev_mode%tape=.FALSE.
+        our_rev_mode%adjoint=.FALSE.
         CALL phi(U,U_IP1,B,H,BETA_FRIC)
-        !Restore the stack pointers
-        double_tape_pointer = temp_double_tape_pointer   
-        integer_tape_pointer = temp_integer_tape_pointer     
-        logical_tape_pointer = temp_logical_tape_pointer    
-        character_tape_pointer = temp_character_tape_pointer 
-        stringlength_tape_pointer = temp_stringlength_tape_pointer
       endif 
       if(isinloop.eq.2 ) then
+        !print *, "FW:2-1", double_tape_pointer, integer_tape_pointer, our_rev_mode%plain, our_rev_mode%tape, our_rev_mode%adjoint
         CALL phi(U,U_IP1,B,H,BETA_FRIC)
+        !print *, "FW:2-2", double_tape_pointer, integer_tape_pointer, our_rev_mode%plain, our_rev_mode%tape, our_rev_mode%adjoint
       endif 
       our_rev_mode=our_orig_mode
     end if
     if (our_rev_mode%adjoint) then
+      !If isinloop==0, then call phi(..) in once
+      !If isinloop==1, then call phi(..) in till convergence, using the same partials on the stack each time
+      !If isinloop==2, then call phi(..) in once 
       our_orig_mode=our_rev_mode
       if(isinloop.eq.2) then
         CONV_FLAG = .false.
@@ -134,7 +133,9 @@ integer myi
           call pop_s0(U_DUMMY(myi)%d)
         end do
 ! adjoint
+        !print *, "BW:1  ", double_tape_pointer, integer_tape_pointer, our_rev_mode%plain, our_rev_mode%tape, our_rev_mode%adjoint
         CALL phi(U,U_IP1,B,H,BETA_FRIC)
+        !print *, "BW:0  ", double_tape_pointer, integer_tape_pointer, our_rev_mode%plain, our_rev_mode%tape, our_rev_mode%adjoint
       end if
       if(isinloop.eq.1) then
         if(ADJ_CONV_FLAG.eqv..false.) then
@@ -180,8 +181,10 @@ integer myi
         end if
       end if
       if(isinloop.eq.2) then
+        !print *, "BW:2-2", double_tape_pointer, integer_tape_pointer, our_rev_mode%plain, our_rev_mode%tape, our_rev_mode%adjoint
 ! adjoint
         CALL phi(U,U_IP1,B,H,BETA_FRIC)
+        !print *, "BW:2-1", double_tape_pointer, integer_tape_pointer, our_rev_mode%plain, our_rev_mode%tape, our_rev_mode%adjoint
         do myi=1,n+1
           call push_s0(U(myi)%d)
         end do
