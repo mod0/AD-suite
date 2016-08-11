ca07ada8700f12596a3770f20f3a1b9479e99e12
TBosse <work@torsten-bosse.de>
2016-08-11 12:37:14 -0500

    Porous media- original f90 code: Started to restructure the porous media example....

--- Code/experimental/porous_media/f90/original_code/eval_original_code.f90
+++ Code/experimental/porous_media/f90/original_code/eval_original_code.f90
@@ -8,78 +8,226 @@ program runspe10
 
   implicit none
 
-  integer :: i, j, k, ndof
+  ! User-Application specific
+  ! ===========================
+  integer :: i, j, k
   integer :: nx, ny, nz
   integer :: nd, st, pt
-
-  ! input/intermediate variables
-  double precision, dimension(:), allocatable :: mu
-  double precision, dimension(:), allocatable :: sigma
   double precision, dimension(:), allocatable :: Q
   double precision, dimension(:), allocatable :: S
   double precision, dimension(:, :, :), allocatable :: P
   double precision, dimension(:, :, :, :), allocatable :: V
-
-  ! output variables
   double precision :: totaloil
   double precision, dimension(:), allocatable :: Tt
   double precision, dimension(:, :), allocatable :: Pc
+
+  
+
+  ! Standard AD-Suite Interface
+  ! ===========================
+
+  ! dimension for independent, dependent, and the parameter  
+  integer :: n_dim, m_dim, p_dim
+  ! input variables
+  double precision, dimension(:), allocatable :: x
+  ! output variables
+  double precision, dimension(:), allocatable :: y
+  ! parameter variables
+  double precision, dimension(:), allocatable :: param
 
   ! read the command line argument for the location of the data files
   integer :: iargs
   character(len = 100) :: data_directory
   character(len = 100) :: results_directory
+  
+  iargs = iargc()
+  
+  call allocate_independent_variables( n_dim, x    )
+  call allocate_dependent_variables(   m_dim, y    )
+  call allocate_parameter_variables(   p_dim, param)
 
+  call initialize_independent_variables( n_dim, x     )
+  call initialize_parameter_variables(   p_dim, param )
+
+  call evaluate_original_code(n_dim, m_dim, p_dim, x, y, param)
+
+  call save_dependent_variables( m_dim, y )
+
+  call deallocate_independent_variables( n_dim, x )
+  call deallocate_dependent_variables(   m_dim, y )
+  call deallocate_parameter_variables(    p_dim, param )
+  
+  return
+contains
+
+
+
+
+subroutine allocate_independent_variables( n_dim, x)
+  integer, intent(out):: n_dim
+  double precision, dimension(:), allocatable, intent(out):: x
+  ! User-Application specific
+  ! ===========================
+  n_dim = 6
+  ! Standard AD-Suite Interface
+  ! ===========================
+  allocate( x(n_dim) )
+end subroutine allocate_independent_variables
+
+subroutine allocate_dependent_variables( m_dim, y)
+  integer, intent(out):: m_dim
+  double precision, dimension(:), allocatable, intent(out):: y
+  ! User-Application specific
+  ! ===========================
+  m_dim = 1
+  ! Standard AD-Suite Interface
+  ! ===========================
+  allocate( y(m_dim) )
+end subroutine allocate_dependent_variables
+
+subroutine allocate_parameter_variables( p_dim, param)
+  integer, intent(out):: p_dim
+  double precision, dimension(:), allocatable, intent(out):: param
+  ! User-Application specific
+  ! ===========================
   iargs = iargc()
   if(iargs /= 2) then
      write(*,*) "Incorrect number of arguments passed. Expected data and results directory"
   endif
-
   call getarg(1, data_directory)
   call getarg(2, results_directory)
-  
   write(*,*) "Data directory: ", data_directory
   write(*,*) "Results directory: ", results_directory
-
-
-  ! Not passing shared constants present in the parameters file.
   call initialize_scenario(data_directory, nx, ny, nz, st, pt, nd, solver_inner, solver_outer)
-
-  ! allocate arrays
   call allocate_arrays(nx, ny, nz, st, pt, nd, Q, S, P, V, Tt, Pc)
   call allocate_shared_arrays(nx, ny, nz, st, pt, nd)
+  p_dim = 0
+  ! Standard AD-Suite Interface
+  ! ===========================
+  allocate( param(p_dim) )
+end subroutine allocate_parameter_variables
 
-  ! initialize arrays
+subroutine initialize_independent_variables(n_dim, x)
+  integer, intent(in):: n_dim
+  double precision, dimension(:), allocatable, intent(inout):: x
+  ! User-Application specific
+  ! ===========================
+  x(1) = -1.0d0
+  x(2) =  0.0d0
+  x(3) = +1.0d0
+  x(4) = 0.0001d0
+  x(5) = 0.0001d0
+  x(6) = 0.0001d0
+  ! Standard AD-Suite Interface
+  ! ===========================  
+end subroutine initialize_independent_variables
+
+subroutine initialize_parameter_variables(p_dim, param)
+  integer, intent(in):: p_dim
+  double precision, dimension(:), allocatable, intent(inout):: param
+  ! User-Application specific
+  ! ===========================
   call initialize_arrays(Q, S, P, V, Tt, Pc)
   call initialize_shared_arrays(data_directory, nx, ny, nz, st, pt, nd)  ! will not be reinitialized
+  ! Standard AD-Suite Interface
+  ! ===========================
+end subroutine initialize_parameter_variables
+
+subroutine evaluate_original_code(n_dim, m_dim, p_dim, x, y, param)
+  integer :: n_dim, m_dim, p_dim
+  double precision, dimension(n_dim) :: x
+  double precision, dimension(m_dim) :: y
+  double precision, dimension(p_dim) :: param
   
-  ! initialize scalar inputs and outputs
-  ndof = 3
-  allocate( mu(ndof)    )
-  allocate( sigma(ndof) )    
-  mu(1)    = -1.0d0
-  mu(2)    =  0.0d0
-  mu(3)    = +1.0d0
-  sigma(1) = 0.0001d0
-  sigma(2) = 0.0001d0
-  sigma(3) = 0.0001d0
+  integer :: iargs
+  character(len = 100) :: data_directory
+  character(len = 100) :: results_directory
+  double precision, dimension(:), allocatable :: mu
+  double precision, dimension(:), allocatable :: sigma
+  allocate( mu(n_dim/2)    )
+  allocate( sigma(n_dim/2) )    
+  
+  mu(1)    = x(1)
+  mu(2)    = x(2)
+  mu(3)    = x(3)
+  sigma(1) = x(4)
+  sigma(2) = x(5)
+  sigma(3) = x(6)
 
   ! solver verbose parameter, inner and outer iterations are read from
   ! command file.
   verbose = .false.              ! Verbose solver output  
 
-  call wrapper(nx, ny, nz, nd, ndof, pt, st, mu, sigma, Q, S, P, V, Tt, Pc, totaloil)
+  call wrapper(nx, ny, nz, nd, ndim/2, pt, st, mu, sigma, Q, S, P, V, Tt, Pc, totaloil)
+  y(1) = totaloil
   
-  ! write results
-  call write_results(results_directory, nx, ny, nz, ndof, mu, sigma, Tt, Pc, totaloil)
-
-  call deallocate_arrays(Q, S, P, V, Tt, Pc)
-  call deallocate_shared_arrays()
   deallocate(mu)
   deallocate(sigma)
-  return
-contains
+end subroutine evaluate_original_code
 
+subroutine save_dependent_variables(m_dim, y)
+  integer, intent(in):: m_dim
+  double precision, dimension(:), allocatable, intent(in):: y
+  ! Standard AD-Suite Interface
+  ! ===========================  
+  call write_results(results_directory, nx, ny, nz, ndof, mu, sigma, Tt, Pc, totaloil)
+  ! Standard AD-Suite Interface
+  ! ===========================  
+end subroutine save_dependent_variables
+
+subroutine deallocate_independent_variables( n_dim, x)
+  integer, intent(in):: n_dim
+  double precision, dimension(:), allocatable, intent(inout):: x
+  ! User-Application specific
+  ! ===========================
+  ! Standard AD-Suite Interface
+  ! ===========================  
+  deallocate( x )
+end subroutine deallocate_independent_variables
+
+subroutine deallocate_dependent_variables( m_dim, y)
+  integer, intent(in):: m_dim
+  double precision, dimension(:), allocatable, intent(inout):: y
+  ! User-Application specific
+  ! ===========================
+  ! Standard AD-Suite Interface
+  ! ===========================  
+  deallocate( y )
+end subroutine deallocate_dependent_variables
+
+subroutine deallocate_parameter_variables( p_dim, param)
+  integer, intent(in):: p_dim
+  double precision, dimension(:), allocatable, intent(inout):: param
+  ! User-Application specific
+  ! ===========================
+  call deallocate_arrays(Q, S, P, V, Tt, Pc)
+  call deallocate_shared_arrays()
+  ! Standard AD-Suite Interface
+  ! ===========================  
+  deallocate( param )
+end subroutine deallocate_parameter_variables
+
+
+  
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ! The following subroutines should all be in the src folder!!!!!!!!!
   subroutine allocate_shared_arrays(nx, ny, nz, st, pt, nd)
     integer :: nx, ny, nz
     integer :: st, pt, nd
